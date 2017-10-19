5a76d5f8ca6241ba05710a9085932b8975260f49
KetillG <ketillgudmunds+github@gmail.com>
2017-10-19 01:21:08 +0000

    Ignore last, this works

--- webpack.config.js
+++ webpack.config.js
@@ -22,10 +22,10 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ]
 };
-new webpack.DefinePlugin({
-    'process.env': {
-      'NODE_ENV': JSON.stringify('production')
-    }
-  })
